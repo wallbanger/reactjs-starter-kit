34d773b13ef23e613fdd992ea1f4a8bc0ac6d2b4
Eugene Koval <iam.eugenekoval@gmail.com>
2016-06-17 00:44:27 +0300

    update webpack and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,14 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.jsx?/, loaders: ['babel'], include: path.join(__dirname, 'src') },
-            { test: /\.css$/, loaders: ['style', 'css?modules&importLoaders=1', 'postcss-loader'] },
             {
+                test: /\.jsx?/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src')
+            }, {
+                test: /\.css$/,
+                loaders: ['style', 'css?modules&importLoaders=1']
+            }, {
                 test: /\.scss$/,
                 loaders: [
                     'style?sourceMap',
@@ -26,11 +31,6 @@ module.exports = {
             }
         ]
     },
-    postcss: [
-        require('postcss-import'),
-        require('postcss-nested'),
-        require('postcss-custom-properties')
-    ],
     watch: true,
     watchOptions: {
         aggregateTimeout: 100
