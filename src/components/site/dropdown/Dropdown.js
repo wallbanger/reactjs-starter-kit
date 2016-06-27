68db41d0871bc492bcf5d1d2dff8c0a4b2df8b89
Eugene Koval <iam.eugenekoval@gmail.com>
2016-06-27 20:53:29 +0300

    update gitinore and dropdown.js

--- src/components/site/dropdown/Dropdown.js
+++ src/components/site/dropdown/Dropdown.js
@@ -24,6 +24,9 @@ class Dropdown extends Component {
           <li><a href="#">Item 1</a></li>
           <li><a href="#">Item 2</a></li>
           <li><a href="#">Item 3</a></li>
+          <li><a href="#">Item 4</a></li>
+          <li><a href="#">Item 5</a></li>
+          <li><a href="#">Item 6</a></li>
         </ul>
       </div>
     );
